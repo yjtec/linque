9ca6c509737a677b7fc5d0a709aa084e8b037193
阿鑫 <18716463@qq.com>
2020-08-20 20:06:31 +0800

    进程名改为小写

--- src/Worker/Master.php
+++ src/Worker/Master.php
@@ -45,7 +45,7 @@ class Master {
 //        $this->registerSigHandlers();
             //此处已经是子进程的子进程了,可以在此处进行下一步逻辑了
             $this->displayUI(); //显示方框
-            $procTitle = 'Linque '. implode(' ',$_SERVER['argv']);
+            $procTitle = 'linque '. implode(' ',$_SERVER['argv']);
             while (1) {
                 foreach ($this->Que as &$queue) {
                     if (!$queue['pid']) {
@@ -54,7 +54,7 @@ class Master {
                             cli_set_process_title($procTitle . ' : ' . $queue['que']);
                             return $this->slaverMonitor($queue['que']); //子进程
                         } elseif ($pid > 0) {
-                            cli_set_process_title($procTitle . ' : Master');
+                            cli_set_process_title($procTitle . ' : master');
                             $queue['pid'] = $pid;
                             $this->procLine->EchoAndLog("创建子进程成功Pid=" . $pid . ',监控队列' . $queue['que'] . PHP_EOL);
                         } else {
